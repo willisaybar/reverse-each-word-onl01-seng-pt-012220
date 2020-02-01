b025954e6d1cd8c070358af9baac44fd7b9054a7
Willis Aybar <willisaybar@gmail.com>
2020-02-01 22:03:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-    sentence.split
+    sentence.split << sentence
+    sentence.each do 
+      sentence.reverse
+    end
+    sentence.reverse
 end
