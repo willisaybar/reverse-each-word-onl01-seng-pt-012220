80fe056d73cb2ac634b302838732d3e832b7a869
Willis Aybar <willisaybar@gmail.com>
2020-02-01 21:55:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  
-  return sentence.each do 
-    sentence.reverse "Hello there, and how are you?"
-  end
+  sentence.split
+  return sentence.reverse 
 end
